1e6d68790d643317fe4a91d3070270215e2e9e19
endo9000 <deejayfromthailand+github@gmail.com>
2024-05-03 16:58:54 +0700

    update

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# # views.py
-# import requests
+# views.py
+import requests
 
 
-# def get_model_file(model: str):
-#     base_url = "http://localhost:11434"
-#     api_url = f"{base_url}/api/show"
-#     data = {"name": model}
-#     response = requests.post(api_url, json=data)
-#     modelfile = response.json()['modelfile']
-#     lines = modelfile.split('\n')
-#     filtered_lines = [line for line in lines if not line.lstrip().startswith('#')]
-#     return '\n'.join(filtered_lines)
+def get_model_file(model: str):
+    base_url = "http://localhost:11434"
+    api_url = f"{base_url}/api/show"
+    data = {"name": model}
+    response = requests.post(api_url, json=data)
+    modelfile = response.json()['modelfile']
+    lines = modelfile.split('\n')
+    filtered_lines = [line for line in lines if not line.lstrip().startswith('#')]
+    return '\n'.join(filtered_lines)
 
-# def main():
-#     response = get_model_file("gemma:2b")
-#     print(response)
+def main():
+    response = get_model_file("gemma:2b")
+    print(response)
     
     
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
