4d851d781e6613beafd74fbd76254e90afe6d1ea
endo9000 <deejayfromthailand+github@gmail.com>
2024-05-27 06:00:02 +0700

    updated sort button to finally work. thanks seb!

--- backend/model_manager/static/js/ollamoma.js
+++ backend/model_manager/static/js/ollamoma.js
@@ -1,132 +1,125 @@
 document.addEventListener("alpine:init", () => {
-    Alpine.data("modelData", () => ({
-
+  Alpine.data("modelData", () => ({
     searchNeedle: "",
-    modelList: JSON.parse(document.getElementById("model_list").textContent).sort((a, b) => {
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-        return 0;
-    }),
+    modelList: JSON.parse(document.getElementById("model_list").textContent).sort((a, b) => a.name.localeCompare(b.name)),
+    sortBy: "name",
+    sortDirection: 1,
+    nameDescending: false,
+    filesizeDescending: false,
+    paramsizeDescending: false,
+    lastdateDescending: true,
 
-    name_decending: false,
-    sort_by_name() {
-        const direction = this.name_decending? -1 : 1;
-        this.modelList.sort((a, b) => {
-            if (a.name < b.name) return direction;
-            if (a.name > b.name) return -direction;
-            return 0;
-        });
-        this.name_decending = !this.name_decending;
-    },
-    
-    filesize_decending: false,
-    sort_by_filesize() {
-        const direction = this.filesize_decending? -1 : 1;
-        this.modelList.sort((a, b) => {
-            if (a.size < b.size) return direction;
-            if (a.size > b.size) return -direction;
-            return 0;
-        });
-        this.filesize_decending = !this.filesize_decending;
-    },
-
-    paramsize_decending: false,
-    sort_by_paramsize() {
-        const direction = this.paramsize_decending? -1 : 1;
-        this.modelList.sort((a, b) => {
-            if (a.parameter_size < b.parameter_size) return direction;
-            if (a.parameter_size > b.parameter_size) return -direction;
-            return 0;
-        });
-        this.paramsize_decending = !this.paramsize_decending;
+    convertParamsToNumeric(param) {
+      if (!param) return 0
+      const unit = param[param.length - 1]
+      let value = parseFloat(param.slice(0, -1))
+      switch (unit) {
+        case "M":
+          value *= 1000000
+          break
+        case "B":
+          value *= 1000000000
+          break
+        case "K":
+          value *= 1000
+          break
+        default:
+          console.log("Could not process parameter size:", param)
+          return 0
+      }
+      return Math.floor(value)
     },
 
-    lastdate_decending: true,
-    sort_by_lastmod() {
-        const direction = this.lastdate_decending? -1 : 1;
-        this.modelList.sort((a, b) => {
-            if (a.modified_at < b.modified_at) return direction;
-            if (a.modified_at > b.modified_at) return -direction;
-            return 0;
-        });
-        this.lastdate_decending = !this.lastdate_decending;
-    },
-    
     formatDate(dateString) {
-        const isoString = `last modified: ${dateString}`;
-        const date = new Date(isoString.substring(15));
-        const now = new Date();
-        const ago = Math.floor((now - date) / (1000 * 3600 * 24));
-        let formattedDate = date.toLocaleDateString();
-        if (ago === 0) {
-            formattedDate += ' (today)';
-        } else if (ago === 1) {
-            formattedDate += ` (1 day ago)`;
-        } else {
-            formattedDate += ` (${ago} days ago)`;
-        }
-        return formattedDate;
-    },
-    
-    formatSize(size) {
-        const bytes = parseInt(size);
-        const mbSize = bytes / (1024 * 1024);
-        const gbSize = mbSize / 1024;
-        if (gbSize >= 1) {
-        return `${gbSize.toFixed(2)} GB`;
-        } else {
-        return `${mbSize.toFixed(2)} MB`;
-        }
+      const date = new Date(dateString)
+      const now = new Date()
+      const ago = Math.floor((now - date) / (1000 * 3600 * 24))
+      let formattedDate = date.toLocaleDateString()
+      if (ago === 0) {
+        formattedDate += " (today)"
+      } else if (ago === 1) {
+        formattedDate += ` (1 day ago)`
+      } else {
+        formattedDate += ` (${ago} days ago)`
+      }
+      return formattedDate
     },
 
-    fetchModelFile: (model) => {
-        if (model.modelFile === null) {
-            fetch(`get-model-file/${model.name}/`)
-                .then((response) => {
-                    return response.text();
-                })
-                .then((file) => {
-                    model.modelFile = file;
-                });
-        }
+    formatSize(size) {
+      const bytes = parseInt(size)
+      const mbSize = bytes / (1024 * 1024)
+      const gbSize = mbSize / 1024
+      return gbSize >= 1 ? `${gbSize.toFixed(2)} GB` : `${mbSize.toFixed(2)} MB`
+    },
+
+    sort(by) {
+      this.sortBy = by
+      switch (by) {
+        case "name":
+          this.modelList.sort((a, b) => a.name.localeCompare(b.name) * this.sortDirection)
+          this.nameDescending = !this.nameDescending
+          break
+        case "filesize":
+          this.modelList.sort((a, b) => this.filesizeDescending ? a.size-b.size : b.size-a.size)
+          this.filesizeDescending = !this.filesizeDescending
+          break
+        case "paramsize":
+          this.modelList.sort((a, b) => this.paramsizeDescending 
+            ? this.convertParamsToNumeric(a.details.parameter_size) - this.convertParamsToNumeric(b.details.parameter_size)
+            : this.convertParamsToNumeric(b.details.parameter_size) - this.convertParamsToNumeric(a.details.parameter_size))
+          this.paramsizeDescending = !this.paramsizeDescending
+          break
+        case "lastmod":
+          this.modelList.sort((a, b) => a.modified_at.localeCompare(b.modified_at) * this.sortDirection)
+          this.lastdateDescending = !this.lastdateDescending
+          break
+      }
+      this.sortDirection = this.sortDirection * -1
+    },
+
+    fetchModelFile(model) {
+      if (!model.modelFile) {
+        fetch(`get-model-file/${model.name}/`)
+          .then(response => response.text())
+          .then(file => {
+            model.modelFile = file
+          })
+      }
     },
 
     copyModel(modelName, newModelName) {
-        fetch(`/copy-model/${modelName}/${newModelName}/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': document.cookie.split("=")[1] // Add CSRF token to DELETE request
-            },
-        });
+      fetch(`/copy-model/${modelName}/${newModelName}/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": document.cookie.split("=")[1],
+        },
+      })
     },
-    
+
     deleteModel(modelName) {
-        fetch(`/delete-model/${modelName}/`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': document.cookie.split("=")[1] // Add CSRF token to DELETE request
-            },
-        }).catch((e) => {
-            console.error("ERROR:", e);
-        }).then(() => {            
-            console.log(`${modelName} deleted!`);
-            document.getElementById(modelName).remove();
-        });
+      fetch(`/delete-model/${modelName}/`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": document.cookie.split("=")[1],
+        },
+      })
+        .catch(e => console.error("ERROR:", e))
+        .then(() => {
+          console.log(`${modelName} deleted!`)
+          document.getElementById(modelName).remove()
+        })
     },
 
     renameModel(modelName, newModelName) {
-        copyModel(modelName, newModelName)
-            .then(() => {
-                deleteModel(modelName);
-            });
+      this.copyModel(modelName, newModelName).then(() => {
+        this.deleteModel(modelName)
+      })
     },
+  }))
 
-
-    }));
-
-    Alpine.data("textareaData", () => ({
+  Alpine.data("textareaData", () => ({
     counter: 0,
-    }));
-});
+  }))
+})
