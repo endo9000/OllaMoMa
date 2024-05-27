6ee80626634b5f492ea1b82719f946d0402e8356
endo9000 <deejayfromthailand+github@gmail.com>
2024-05-27 07:45:30 +0700

    modelfile save/undo added/finished

--- backend/model_manager/urls.py
+++ backend/model_manager/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('copy-model/<str:model_name>/<str:new_model_name>/', views.copy_model, name='copy-model'),
     path('delete-model/<str:model_name>/', views.delete_model, name='delete-model'),
     path('get-model-file/<str:model_name>/', views.get_model_file, name='get-model-file'),
+    path('save-model-file/<str:model_name>', views.save_model_file, name='save-model-file'),
 ]
