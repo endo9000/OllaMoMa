3bf9ef708864fee63490cc9eef39e7715f3776c3
endo9000 <deejayfromthailand+github@gmail.com>
2024-07-04 10:16:06 +0700

    first commit: working shadcn-vue project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+    <Button variant="ghost">Click me</Button>
+</template>
+
+<style scoped>
+
+</style>
